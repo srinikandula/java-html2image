2c50b095a2aec3c537d88f3e9f9c8f98566da273
srinikandula <srini.kandula@whizzard.in>
2020-12-31 13:01:33 +0530

    adding new samples

--- html2image/src/test/java/gui/ava/html/Example.java
+++ html2image/src/test/java/gui/ava/html/Example.java
@@ -1,17 +1,22 @@
 package gui.ava.html;
 
-import gui.ava.html.image.*;
+import gui.ava.html.image.HtmlImageGenerator;
+import org.springframework.util.ResourceUtils;
+
+import java.io.FileNotFoundException;
+import java.net.URL;
 
 /**
  * Created by hki on 07-01-2016.
  */
 public class Example {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        URL url = ResourceUtils.getURL("classpath:id_card_template.html");
         HtmlImageGenerator imageGenerator = new HtmlImageGenerator();
-        imageGenerator.loadHtml("<b>Hello World!</b> Please goto <a title=\"Goto Google\" href=\"http://www.google.com\">Google</a>.");
-        imageGenerator.saveAsImage("hello-world.png");
-        imageGenerator.saveAsImage("hello-world.jpeg");
+        imageGenerator.loadUrl(url);
+        imageGenerator.saveAsImage("id_card.png");
+        imageGenerator.saveAsImage("id_card.jpeg");
         //imageGenerator.saveAsHtmlWithMap("hello-world.html", "hello-world.png");
     }
 }
